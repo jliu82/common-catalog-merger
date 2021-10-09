9f638e7ed0de52e5d8230fbc0d7748f711e1b4fc
Jay Liu <j33apple@hotmail.com>
2021-10-09 18:04:41 +1100

    Added logic for merging products

--- pages/api/category-merger.js
+++ pages/api/category-merger.js
@@ -1,28 +1,91 @@
-import multer from 'multer';
+import multer from "multer";
+import {get, set, find, assign, forEach} from "lodash"
 
-const upload = multer()
+const upload = multer();
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-        upload.array("files", 6)(req, {}, err => {
-            if(err){
-                res.status(400).json({ error: err })
-            }
-            req.files.map(file=>{
-                console.log(file.originalname)
-                console.log(file.buffer.toString())
-            })
-          })
-          res.status(200).send({})
-          
-    }else{
-        res.status(200).json({ status: 'it is running' })
-    }
+  if (req.method === "POST") {
+    upload.array("files", 6)(req, {}, (err) => {
+      if (err) {
+        res.status(400).json({ error: err });
+      }
+      generateMergedProducts(processFiles(req.files));
+    });
+    res.status(200).send({});
+  } else {
+    res.status(200).json({ status: "it is running" });
+  }
 }
 
+export const processFiles = (files) => {
+  const companies = {};
+  files.forEach((file) => {
+    //generate path by checking substr of filename (eg catalog*.csv) is A or B, and followed by entity type
+    set(companies, 
+        `${file.originalname.substr(-5, 1).toLowerCase()}.${file.originalname.substr(0,file.originalname.length - 5).toLowerCase()}`,
+        parseCsvContent(file.buffer.toString()))
+  });
+  return companies;
+};
+
+export const parseCsvContent = (data) => {
+  // Split into header and rows
+  const [headerRow, ...rows] = data.split("\n");
+  // Split header/rows with comma seperator into array of values
+  const parseRow = (row) => row.replace("\r", "").split(",");
+  const headers = parseRow(headerRow);
+
+  // Create categories from rows
+  return rows.map((row) =>
+    parseRow(row)
+      // Reduce values array into an object like: { [header]: value }
+      .reduce(
+        (object, value, index) => ({
+          ...object,
+          [headers[index].toLowerCase()]: value.trim(),
+        }),
+        {}
+      )
+  );
+};
+
+export const flagProductsForMerge = (companies)=>{
+    //check each product and flag if it needs to be merged
+    return get(companies, "b.barcodes").reduce((items, barcodeData)=>{
+        const sku = get(barcodeData, "sku");
+        const barcode = get(barcodeData, "barcode")
+         //skip items marked as merge=false which means it has already been found from a's barcodes
+        if(get(items, sku) !== false){
+            set(items, sku, !find(get(companies, "a.barcodes"),(aBarcode)=>aBarcode.barcode === barcode));
+        }
+        return items;
+    }, {})
+}
+
+export const generateMergedProducts = (companies)=>{
+    //collect all the catelog items from a first
+    const allProducts = get(companies, "a.catalog").map(catelog => 
+        {return assign(catelog, {"source" : "a"})}
+    )
+    
+    const itemsToMerge = flagProductsForMerge(companies)
+    //add catelog items from B which has been marked as to be merged
+    forEach(itemsToMerge, (value, key)=>{
+        if(value === true)
+            allProducts.push({"sku":key, "source":"b", "description": findDescriptionBySku(companies.b, key)})
+    })
+
+    return allProducts;
+}
+
+const findDescriptionBySku = (company, sku) =>{
+    return find(get(company, "catalog"),(catelog) => 
+        catelog.sku === sku
+    )
+}
--- pages/api/category-merger.test.js
+++ pages/api/category-merger.test.js
@@ -1,1 +1,104 @@
+import { flagProductsForMerge, processFiles, generateMergedProducts } from "./category-merger";
 
+describe("category merger", () => {
+  const companies = {
+    a: {
+      barcodes: [
+        { supplierid: "00001", sku: "647-vyk-317", barcode: "z2783613083817" },
+        { supplierid: "00001", sku: "647-vyk-317", barcode: "z2783613083817" },
+      ],
+      suppliers: [
+        { id: "00001", name: "Twitterbridge" },
+        { id: "00002", name: "Thoughtsphere" },
+      ],
+      catalog: [
+        { sku: "647-vyk-317", description: "Walkers Special Old Whiskey" },
+      ],
+    },
+    b: {
+      barcodes: [
+        { supplierid: "00001", sku: "947-vyk-317", barcode: "z2783613083817" },
+        { supplierid: "00002", sku: "647-vyk-317", barcode: "z2783613083818" },
+      ],
+      suppliers: [
+        { id: "00001", name: "Wikivu" },
+        { id: "00002", name: "Divavu" },
+      ],
+      catalog: [
+        {
+          sku: "647-vyk-317",
+          description: "Another Walkers Special Old Whiskey",
+        },
+        { sku: "947-vyk-317", description: "Bread - Raisin" },
+      ],
+    },
+  };
+
+  const files = [
+    {
+      fieldname: "files",
+      originalname: "suppliersB.csv",
+      buffer: `ID,Name
+      00001,Twitterbridge
+      00002,Thoughtsphere`,
+    },
+    {
+      fieldname: "files",
+      originalname: "suppliersA.csv",
+      buffer: "1",
+    },
+    {
+      fieldname: "files",
+      originalname: "catalogB.csv",
+      buffer: `SKU,Description
+      647-vyk-317,Walkers Special Old Whiskey
+      280-oad-768,Bread - Raisin`,
+    },
+    {
+      fieldname: "files",
+      originalname: "catalogA.csv",
+      buffer: `SKU,Description
+      647-vyk-317,Walkers Special Old Whiskey
+      280-oad-768,Bread - Raisin`,
+    },
+    {
+      fieldname: "files",
+      originalname: "barcodesB.csv",
+      buffer: "1",
+    },
+    {
+      fieldname: "files",
+      originalname: "barcodesA.csv",
+      buffer: `SupplierID,SKU,Barcode
+      00001,647-vyk-317,z2783613083817
+      00001,647-vyk-317,z2783613083818
+      00001,647-vyk-317,z2783613083819`,
+    },
+  ];
+
+  it("should parse csv contents into companies dataset", () => {
+    const companies = processFiles(files);
+    expect(companies["a"]["catalog"].length).toBe(2);
+    expect(companies["a"]["catalog"][0]["sku"]).toEqual("647-vyk-317");
+    expect(companies["a"]["barcodes"].length).toBe(3);
+    expect(companies["a"]["suppliers"].length).toBe(0);//skipped invalid value "1"
+
+    expect(companies["b"]["barcodes"].length).toBe(0);
+    expect(companies["b"]["suppliers"].length).toBe(2);
+    expect(companies["b"]["catalog"].length).toBe(2);
+  });
+
+  it("should collect products from company B with merge flag", () => {
+    const products = flagProductsForMerge(companies);
+    expect(products["647-vyk-317"]).toBe(true);
+    expect(products["947-vyk-317"]).toBe(false);
+  });
+
+  it("should genearate merged product list", () => {
+    const products = generateMergedProducts(companies);
+    expect(products.length).toBe(2);
+    expect(products[0]["sku"]).toEqual("647-vyk-317");
+    expect(products[0]["description"]).toEqual("Walkers Special Old Whiskey");
+    expect(products[0]["source"]).toEqual("a");
+  });
+});
